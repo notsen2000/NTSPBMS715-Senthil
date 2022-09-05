45c41365c8d66e6b01a428f78bb01779fd0e0d7b
Dev1 <Dev1@gmail.com>
2022-09-05 13:21:47 +0530

    dev1 - m3() code commit

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -17,7 +17,14 @@ public class Utility {
 		System.out.println("Utility.m2()");
 	}
 	
+ 
 	public void m4() {
 		System.out.println("Utility.m4()");
+	}
+ 
+	public void m3() {
+	 System.out.println("Utility.m3()");
+		
+ 
 	}
 }
